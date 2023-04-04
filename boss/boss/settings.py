0f2f44ad4bf9bab8c48b324debbb5fea481c950c
2537681868@qq.com <2537681868@qq.com>
2023-04-04 23:11:25 +0800

    add test models

--- boss/boss/settings.py
+++ boss/boss/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recruitment',
+    'recruitment.apps.RecruitmentConfig',
 ]
 
 MIDDLEWARE = [
--- boss/recruitment/views.py
+++ boss/recruitment/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import json
 
 # Create your views here.
 def index(request):
+    data = {"code": 0, "msg": "ok"}
+    return JsonResponse(data)
+    return HttpResponse(json.dumps(data), content_type="application/json")
     return HttpResponse("test1")
